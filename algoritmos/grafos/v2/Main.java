b03d4a46296bf800bb68f2513091f048e6d43fee
Adriane <rodrigues.adriane@outook.com>
2022-03-16 22:20:31 -0300

    WIP algoritmo de largura mais ajeitado

--- algoritmos/grafos/v2/Main.java
+++ algoritmos/grafos/v2/Main.java
@@ -2,7 +2,41 @@ package algoritmos.grafos.v2;
 
 public class Main {
     public static void main(String[] args) {
-        Node[] node = new Node[]{
+        Graph graph = new Graph(buildEdges());
+
+        System.out.printf("%s", graph);
+
+        findNode(graph, new Node(6));
+    }
+
+    private static void findNode(Graph graph, Node nodeToFind) {
+        Edge[] edges = graph.edges;
+        int[] verifiedNodes = new int[edges.length];
+        int verifiedNodeIndex = 0;
+        for (int i = 0; i < edges.length; i++) {
+            if (!containsNode(verifiedNodes, edges[i].actualNode)) {
+                if (edges[i].actualNode.value == nodeToFind.value) {
+                    System.out.printf("Node %s found at attempt %d", edges[i].actualNode, i);
+                    return;
+                }else {
+                    verifiedNodes[verifiedNodeIndex++] = edges[i].actualNode.value;
+
+                }
+            }
+        }
+    }
+
+    private static boolean containsNode(int[] verifiedNodes, Node node) {
+        for (int i = 0; i < verifiedNodes.length; i++) {
+            if (verifiedNodes[i] == node.value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static Edge[] buildEdges() {
+        Node[] nodes = new Node[]{
                 new Node(1),
                 new Node(2),
                 new Node(3),
@@ -12,17 +46,14 @@ public class Main {
         };
 
         Edge[] edges = new Edge[]{
-                new Edge(node[0], node[4]),
-                new Edge(node[0], node[1]),
-                new Edge(node[4], node[3]),
-                new Edge(node[1], node[2]),
-                new Edge(node[4], node[1]),
-                new Edge(node[3], node[2]),
-                new Edge(node[3], node[5])
+                new Edge(nodes[0], nodes[4]),
+                new Edge(nodes[0], nodes[1]),
+                new Edge(nodes[4], nodes[3]),
+                new Edge(nodes[1], nodes[2]),
+                new Edge(nodes[4], nodes[1]),
+                new Edge(nodes[3], nodes[2]),
+                new Edge(nodes[3], nodes[5])
         };
-
-        Graph graph = new Graph(edges);
-
-        System.out.printf("%s", graph);
+        return edges;
     }
 }
